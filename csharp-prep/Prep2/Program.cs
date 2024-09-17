e40b6bb8a71ad8616083c50b996a00535410286e
Nnatuanya Frank <nnatuanyafrank@gmail.com>
2024-09-17 18:54:03 +0100

    done with prep1 to prep3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,75 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        int lastDigit = grade % 10;
+
+        if (grade >= 90)
+        {   
+            if (lastDigit >= 7)
+            {
+                Console.WriteLine($"A {grade}");
+            }
+            else
+            {
+                Console.WriteLine($"A- {grade}");
+            }
+            
+        }
+
+        else if (grade >= 80)
+        {
+            if (lastDigit >= 7)
+            {
+                Console.WriteLine($"B+ {grade}");
+            }
+            else
+            {
+                Console.WriteLine($"B- {grade}");
+            }
+        } 
+
+        else if (grade >= 70)
+        {
+           if (lastDigit >= 7)
+            {
+                Console.WriteLine($"C+ {grade}");
+            }
+            else
+            {
+                Console.WriteLine($"C- {grade}");
+            }
+        }
+
+        else if (grade >= 60)
+        {
+           if (lastDigit >= 7)
+            {
+                Console.WriteLine($"D+ {grade}");
+            }
+            else
+            {
+                Console.WriteLine($"D- {grade}");
+            }
+        } 
+
+        else if(grade < 60)
+        {
+            Console.WriteLine($"F {grade}");
+        }
+        
+        else
+        {
+            Console.WriteLine("Invalid Input, Please Try Again");
+        }
+
+        if (grade < 70)
+        {
+            Console.WriteLine("Your grade is pretty poor, we trust you can do better next time");
+        }
+        else{
+            Console.WriteLine("Congratulations on Your Success, Keep it Up");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // int magicNumber = 10; 
+        // Console.Write("What is the magic number? ");
+        string response = " ";
+
+        while (response != "no")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int number = 0;
+            int guesses = 0;
+
+            while (magicNumber != number)
+            {
+                Console.Write("What is your guess? ");
+                number = int.Parse(Console.ReadLine());
+                guesses++ ;
+
+                if (number > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (number < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it, right!");
+                    Console.WriteLine($"it took you this number of {guesses}");
+                }
+            }
+
+            Console.Write("Do you want to try again? 'yes' or 'no': ");
+            response = Console.ReadLine();
+
+        }
+
     }
 }
